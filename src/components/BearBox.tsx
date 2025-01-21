5d3f27c5a069272d2d700375ee3f5b6dad21bc1e
luzhihong <xiaopikaqiu@outlook.com>
2025-01-21 10:10:05 +0800

    zustand  basic usage

--- src/components/BearBox.tsx
+++ src/components/BearBox.tsx
@@ -5,13 +5,15 @@ export const BearBox = () => {
   //   const bears = useBearStore((state) => state.bears);
   //   const increasePopulation = useBearStore((state) => state.increasePopulation);
   //   const removeAllBears = useBearStore((state) => state.removeAllBears);
-  const { bears, increasePopulation, removeAllBears } = useBearStore();
+  const { bears, increasePopulation, decreasePopulation, removeAllBears } =
+    useBearStore();
   return (
     <div className="box">
       <h1>Bear Box</h1>
       <p>bears: {bears}</p>
       <div>
         <button onClick={increasePopulation}>add bear</button>
+        <button onClick={decreasePopulation}>del bear</button>
         <button onClick={removeAllBears}>remove all bears</button>
       </div>
     </div>
--- src/stores/bearStore.tsx
+++ src/stores/bearStore.tsx
@@ -1,10 +1,10 @@
 import { create } from "zustand";
 
-type TBearStoreState = {
-  bears: number;
-  increasePopulation: () => void;
-  removeAllBears: () => void;
-};
+// type TBearStoreState = {
+//   bears: number;
+//   increasePopulation: () => void;
+//   removeAllBears: () => void;
+// };
 
 export const useBearStore = create<TBearStoreState>()((set) => ({
   bears: 0,
@@ -12,5 +12,24 @@ export const useBearStore = create<TBearStoreState>()((set) => ({
     set((state) => ({
       bears: state.bears + 1,
     })),
+  decreasePopulation: () => {
+    set((state) => ({
+      bears: state.bears - 1,
+    }));
+  },
   removeAllBears: () => set({ bears: 0 }),
+  getOwner: async () => {
+    const result = await fetch("https://api.github.com/users/1");
+    const owner = await result.json();
+    console.log(owner.name);
+    return owner.name;
+  },
 }));
+
+type TBearStoreState = {
+  bears: number;
+  increasePopulation: () => void;
+  decreasePopulation: () => void;
+  removeAllBears: () => void;
+  getOwner: () => Promise<string>;
+};

95aa4ecfeffb7210eb27f6b1478f4233624307d5
luzhihong <xiaopikaqiu@outlook.com>
2025-01-22 09:44:46 +0800

    shallow function

--- src/components/CatController.tsx
+++ src/components/CatController.tsx
@@ -1,9 +1,24 @@
+import { shallow } from "zustand/shallow";
 import { useCatStore } from "../stores/catStore";
 
 const CatController = () => {
   //   const {  increaseBigCats, increaseSmallCats,  } = useCatStore();
-  const increaseBigCats = useCatStore.use.increaseBigCats();
-  const increaseSmallCats = useCatStore.use.increaseSmallCats();
+  // const increaseBigCats = useCatStore.use.increaseBigCats();
+  // // const increaseSmallCats = useCatStore.use.increaseSmallCats();
+
+  // const { increaseBigCats, increaseSmallCats } = useCatStore(
+  //   (state) => ({
+  //     increaseBigCats: state.increaseBigCats,
+  //     increaseSmallCats: state.increaseSmallCats,
+  //   }),
+  //   shallow
+  // );
+
+  const [increaseBigCats, increaseSmallCats] = useCatStore(
+    (state) => [state.increaseBigCats, state.increaseSmallCats],
+    shallow
+  );
+
   return (
     <div className="box">
       <h1>Cat Controller</h1>

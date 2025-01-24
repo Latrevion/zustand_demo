dd74cf45e2d23b7004ffeeef0fe49680a5555539
luzhihong <xiaopikaqiu@outlook.com>
2025-01-24 10:59:11 +0800

    zustand typescript

--- src/stores/catStore.ts
+++ src/stores/catStore.ts
@@ -1,5 +1,5 @@
 import { createSelectors } from "./../utils/createSelectors";
-import { create } from "zustand";
+import { create, StateCreator } from "zustand";
 import { devtools, persist, subscribeWithSelector } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
@@ -10,34 +10,39 @@ type TCatStoreState = {
   summary: () => number;
 };
 
+const createCatSlice: StateCreator<
+  TCatStoreState,
+  [
+    ["zustand/immer", never],
+    ["zustand/devtools", unknown],
+    ["zustand/subscribeWithSelector", never],
+    ["zustand/persist", unknown]
+  ]
+> = (set, get) => ({
+  cats: {
+    smallCats: 0,
+    bigCats: 0,
+  },
+  increaseBigCats: () =>
+    set((state) => {
+      state.cats.bigCats++;
+    }),
+  increaseSmallCats: () =>
+    set((state) => {
+      state.cats.smallCats++;
+    }),
+
+  summary: () => {
+    const total = get().cats.bigCats + get().cats.smallCats;
+    return total;
+  },
+});
+
 export const useCatStore = createSelectors(
   create<TCatStoreState>()(
     immer(
       devtools(
-        subscribeWithSelector(
-          persist(
-            (set, get) => ({
-              cats: {
-                smallCats: 0,
-                bigCats: 0,
-              },
-              increaseBigCats: () =>
-                set((state) => {
-                  state.cats.bigCats++;
-                }),
-              increaseSmallCats: () =>
-                set((state) => {
-                  state.cats.smallCats++;
-                }),
-
-              summary: () => {
-                const total = get().cats.bigCats + get().cats.smallCats;
-                return total;
-              },
-            }),
-            { name: "cat store" }
-          )
-        ),
+        subscribeWithSelector(persist(createCatSlice, { name: "cat store" })),
         { enabled: true, name: "catStore" }
       )
     )

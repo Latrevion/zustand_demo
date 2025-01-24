78b980ec3d8cd411dc10ddab6d1ec75ec56549ed
luzhihong <xiaopikaqiu@outlook.com>
2025-01-24 10:30:57 +0800

    fix err catStore.ts

--- src/stores/catStore.ts
+++ src/stores/catStore.ts
@@ -36,9 +36,10 @@ export const useCatStore = createSelectors(
               },
             }),
             { name: "cat store" }
-          ),
-          { enabled: true, name: "catStore" }
-        ))
+          )
+        ),
+        { enabled: true, name: "catStore" }
+      )
     )
   )
 );
